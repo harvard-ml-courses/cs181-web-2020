2961247621e6e06d60b30bf319a0ea258227b752
Mark <goldstein.marik@gmail.com>
2018-01-04 18:06:19 -0500

    .

--- update.sh
+++ update.sh
@@ -5,4 +5,5 @@ git commit -m "."
 git push
 
 # This pushes to http://cs181.fas.harvard.edu/
+# TODO: why doesn't a simple pull work? Why does it try to merge?
 ssh cs181@login.fas.harvard.edu "cd public_html; git fetch --all; git reset --hard origin/master; chmod 755 *.html *.css"
