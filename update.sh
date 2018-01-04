adabb3fd45db8a22e0c9562c40b95f571ffe91ff
Mark <goldstein.marik@gmail.com>
2018-01-04 17:31:21 -0500

    .

---
+++ update.sh
@@ -0,0 +1,8 @@
+# This pushes to the repo 
+# and to https://harvard-ml-courses.github.io/cs181-web/
+git add --all
+git commit -m "."
+git push
+
+# This pushes to http://cs181.fas.harvard.edu/
+ssh cs181@login.fas.harvard.edu "cd public_html; git pull; chmod 755 *.html *.css"
