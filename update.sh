503ba8049dfc532e7360aa264860846b04bbf724
Mark <goldstein.marik@gmail.com>
2018-01-04 19:17:08 -0500

    .

--- update.sh
+++ update.sh
@@ -4,6 +4,10 @@ git add --all
 git commit -m "."
 git push
 
+
 # This pushes to http://cs181.fas.harvard.edu/
+# TODO: github automatically reads YAML files, but here
+# we would need to generate a index.html file from a template html
+# and from the YAML file using JINJA.
 # TODO: why doesn't a simple pull work? Why does it try to merge?
-ssh cs181@login.fas.harvard.edu "cd public_html; git fetch --all; git reset --hard origin/master; chmod 755 *.html *.css"
+#ssh cs181@login.fas.harvard.edu "cd public_html; git fetch --all; git reset --hard origin/master; chmod 755 *.html *.css"
