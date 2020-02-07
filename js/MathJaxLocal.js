be00b4828eda73b627dd108c51380f5fe3985bb3
jdhe1120 <jhe1120@gmail.com>
2020-02-07 12:24:12 -0500

    fix local reference

--- js/MathJaxLocal.js
+++ js/MathJaxLocal.js
@@ -15,4 +15,4 @@ MathJax.Hub.Config({
   }
 });
 
-MathJax.Ajax.loadComplete("http://127.0.0.1:4000/cs181-web/js/MathJaxLocal.js");
+MathJax.Ajax.loadComplete("https://harvard-ml-courses.github.io/cs181-web/js/MathJaxLocal.js");
